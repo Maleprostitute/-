2252428baa1acf75e2b359627c79c306f295c855
Xin <xinqianhuang@qq.com>
2020-11-01 21:10:55 +0800

    no message

--- src/views/user/Login.vue
+++ src/views/user/Login.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <hm-header>登录122</hm-header>
+    <hm-header>登录</hm-header>
     <hm-logo></hm-logo>
     <van-form @submit="login">
       <van-field
@@ -22,7 +22,7 @@
         <van-button round block type="info" native-type="submit">登录</van-button>
         <p class="tips">
           没有账号?去
-          <router-link to="/register">注11册</router-link>
+          <router-link to="/register">注册</router-link>
         </p>
       </div>
     </van-form>
